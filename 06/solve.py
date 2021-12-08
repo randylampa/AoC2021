254074753d872254cd9170ab795175871c7bb3c0
René Panák <panak@advertising-media.cz>
2021-12-08 07:42:44 +0100

    added days 7 & 8, updated previous

---
+++ 06/solve.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Aoc 2021 - 06
+
+import sys
+sys.path.append('..')
+import utils
+
+def solve_part_1():
+	fn = 'input' if False else 'input-demo'
+	inpList = utils.read_file_into_list(fn)
+	print(inpList)
+
+	print('Part 1 not solved yet')
+	
+	answer = None
+	
+	print("Answer1 =", answer)
+
+def solve_part_2():
+	print('Part 2 not solved yet')
+	
+	answer = None
+	
+	print("Answer2 =", answer)
+
+def main(args):
+	
+	solve_part_1()
+	
+	solve_part_2()
+	
+	return 0
+
+if __name__ == '__main__':
+	sys.exit(main(sys.argv))
