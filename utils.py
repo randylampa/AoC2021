afac5772d293cbf76368de13b20d74829489e5b6
René Panák <panak@advertising-media.cz>
2021-12-09 12:17:49 +0100

    utils - read lists of numbers from file

--- utils.py
+++ utils.py
@@ -4,15 +4,27 @@
 #  
 
 import sys
-	
+
 def read_file_into_list(name='input', mapfnc = lambda x:x.strip()):
+	"""
+	Reads all lines into list and map mapfnc on each.
+	"""
 	f = open(name, 'r')
 	lines = f.readlines()
 	f.close()
 	return [*map(mapfnc, lines)]
-	
+
 def read_file_into_list_of_ints(name='input'):
+	"""
+	Reads all lines into list and map int(x.strip()) on each.
+	"""
 	return read_file_into_list(name, lambda x: int(x.strip()))
+	
+def read_file_into_lists_of_ints(name='input', mapfnc = lambda x:x.strip()):
+	"""
+	Read 
+	"""
+	return read_file_into_list(name, lambda x: [*map(int, x.strip().split(','))])
 
 def main(args):
 	return 0
