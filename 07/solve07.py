0b7eb5731f70bd0f308006b2bf5e8b9dcc78010b
René Panák <rene.panak@seznam.cz>
2021-12-16 21:23:22 +0100

    solve-07-1

--- 07/solve07.py
+++ 07/solve07.py
@@ -6,19 +6,30 @@
 import sys
 sys.path.append('..')
 import utils
+import statistics
 
 ISSUE=7
 
 def solve_part_1():
-	demo = 1
+	demo = 0
 
 	fn = utils.get_input_file(demo, ISSUE);
 	print(fn)
 	"""Do something here >>>"""
+	ll = utils.read_file_into_lists_of_ints(fn)
+	numbers = ll[0]
+	#~ print(numbers)
+	median = int(statistics.median(numbers))
+	#~ print(median)
+	fuel_cost = 0
+	for number in numbers:
+		cost = abs(median - number)
+		#~ print(number, cost)
+		fuel_cost += cost
+		pass
+	print(fuel_cost)
 
-	print('Part 1 not solved yet')
-	
-	answer = None
+	answer = fuel_cost
 
 	"""<<< Do something here"""
 	utils.print_answer(1, demo, answer)
@@ -31,18 +42,18 @@ def solve_part_2():
 	"""Do something here >>>"""
 
 	print('Part 2 not solved yet')
-	
+
 	answer = None
 
 	"""<<< Do something here"""
 	utils.print_answer(2, demo, answer)
 
 def main(args):
-	
+
 	solve_part_1()
-	
-	solve_part_2()
-	
+
+	#~ solve_part_2()
+
 	return 0
 
 if __name__ == '__main__':
