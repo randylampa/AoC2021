6f8a2f5375198113ae34e2952b9be7a172fa1627
René Panák <rene.panak@seznam.cz>
2021-12-16 21:53:22 +0100

    solve-07-2 - ee

--- 07/solve07.py
+++ 07/solve07.py
@@ -7,6 +7,7 @@ import sys
 sys.path.append('..')
 import utils
 import statistics
+import math
 
 ISSUE=7
 
@@ -20,13 +21,14 @@ def solve_part_1():
 	numbers = ll[0]
 	#~ print(numbers)
 	median = int(statistics.median(numbers))
-	#~ print(median)
+	pos = round(median)
+	print('align pos:', pos)
+
 	fuel_cost = 0
 	for number in numbers:
-		cost = abs(median - number)
+		cost = abs(pos - number)
 		#~ print(number, cost)
 		fuel_cost += cost
-		pass
 	print(fuel_cost)
 
 	answer = fuel_cost
@@ -35,24 +37,40 @@ def solve_part_1():
 	utils.print_answer(1, demo, answer)
 
 def solve_part_2():
-	demo = 1
+	demo = 0
 
 	fn = utils.get_input_file(demo, ISSUE);
 	print(fn)
 	"""Do something here >>>"""
+	ll = utils.read_file_into_lists_of_ints(fn)
+	numbers = ll[0]
+	#~ print(numbers)
+	mean = statistics.mean(numbers)
+	print(mean)
+	pos = math.ceil(mean)
+	print('align pos:', pos)
 
-	print('Part 2 not solved yet')
+	fuel_cost = 0
+	for number in numbers:
+		dist = abs(pos - number)
+		print(number, pos, dist)
+		cost = sum(range(1,dist+1))
+		#~ print(number, cost)
+		fuel_cost += cost
+	print(fuel_cost)
 
-	answer = None
+	answer = fuel_cost
+	# is low	 1495426
+	# is high	86397082
 
 	"""<<< Do something here"""
 	utils.print_answer(2, demo, answer)
 
 def main(args):
 
-	solve_part_1()
+	#~ solve_part_1()
 
-	#~ solve_part_2()
+	solve_part_2()
 
 	return 0
 
